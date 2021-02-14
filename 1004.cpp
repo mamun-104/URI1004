b716329687025df95343ad6bb0393f7258c6e525
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-02-15 00:53:31 +0600

    Add files via upload

---
+++ 1004.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+int main()
+{
+    int a,b, PROD;
+    scanf("%d%d",&a,&b);
+
+    PROD = a*b;
+    printf("PROD = %d\n",PROD);
+
+
+    return 0;
+}
+
+
